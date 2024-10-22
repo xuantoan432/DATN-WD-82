6ad231351dde9f438c6776886673789e25243f5c
phuonggcx123 <phuonglun66666@gmail.com>
2024-10-22 21:58:22 +0700

    create layout seller

---
+++ resources/views/seller/layouts/partials/footer.blade.php
@@ -0,0 +1,1 @@
+<p class="mb-0">Copyright © 2024. All right reserved.</p>
---
+++ resources/views/seller/layouts/partials/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="sidebar-header">
+    <div class="logo-icon">
+        <img src="{{asset('theme/admin/assets/images/logo-icon.png')}}" class="logo-img" alt="">
+    </div>
+    <div class="logo-name flex-grow-1">
+        <h5 class="mb-0">Maxton</h5>
+    </div>
+    <div class="sidebar-close">
+        <span class="material-icons-outlined">close</span>
+    </div>
+</div>
+<div class="sidebar-nav">
+    <!--navigation-->
+    <ul class="metismenu" id="sidenav">
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="material-icons-outlined">home</i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+            <ul>
+                <li><a href=""><i class="material-icons-outlined">arrow_right</i>Analysis</a>
+                </li>
+                <li><a href=""><i class="material-icons-outlined">arrow_right</i>eCommerce</a>
+                </li>
+            </ul>
+        </li>
+
+    </ul>
+    </li>
+    </ul>
+    <!--end navigation-->
+</div>
