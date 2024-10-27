fbaec186f5f75359f949b165a79b82fcc6cc5f85
tmhieu <htran31000@gmail.com>
2024-10-27 22:42:48 +0700

    new posts

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,7 +9,7 @@
 
             <div class="mb-3">
                 <label for="title" class="form-label">Tiêu Đề</label>
-                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" required>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror">
                 @error('title')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -17,7 +17,7 @@
 
             <div class="mb-3">
                 <label for="description" class="form-label">Mô Tả</label>
-                <textarea name="description" class="form-control @error('description') is-invalid @enderror" required></textarea>
+                <textarea name="description" class="form-control @error('description') is-invalid @enderror"></textarea>
                 @error('description')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -25,7 +25,7 @@
 
             <div class="mb-3">
                 <label for="content" class="form-label">Nội Dung</label>
-                <textarea id="content" name="content" class="form-control @error('content') is-invalid @enderror" required></textarea>
+                <textarea id="content" name="content" class="form-control @error('content') is-invalid @enderror"></textarea>
                 @error('content')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -33,17 +33,23 @@
 
             <div class="mb-3">
                 <label for="thumbnail" class="form-label">Ảnh Minh Họa</label>
-                <input type="file" name="thumbnail" class="form-control">
+                <input type="file" name="thumbnail" class="form-control @error('thumbnail') is-invalid @enderror" >
+                @error('thumbnail')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="tags" class="form-label">Tags</label>
-                <select name="tags[]" class="form-control" multiple>
+                <select name="tags[]" class="form-control @error('tags') is-invalid @enderror" multiple>
                     <option value="" disabled selected>Chọn Tags</option> <!-- Placeholder -->
                     @foreach ($tags as $tag)
                         <option value="{{ $tag->id }}">{{ $tag->name }}</option>
                     @endforeach
                 </select>
+                @error('tags')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <button type="submit" class="btn btn-primary">Lưu Bài Viết</button>
