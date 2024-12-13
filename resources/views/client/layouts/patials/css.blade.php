d42a724495c1856c2b3cb15e080af86ad4f3fb1e
toan-personal <toanne432@gmail.com>
2024-12-13 19:41:01 +0700

    Chuyển link cdn và css js về từng file

--- resources/views/client/layouts/patials/css.blade.php
+++ resources/views/client/layouts/patials/css.blade.php
@@ -8,3 +8,20 @@
 
 <link rel="stylesheet" href="/theme/client/css/style.css">
 <link rel="stylesheet" href="/theme/client/css/custom.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" />
+
+<style>
+    .toast-title,
+    .toast-message {
+        font-size: 20px !important;
+    }
+
+    .form-control,
+    .form-select {
+        height: 50px;
+        font-size: 16px;
+        width: 100%;
+        max-width: 500px;
+        margin-top: 10px;
+    }
+</style>
