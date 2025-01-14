d434afb81a2a7da85d46bb1884e1de2aa74f138b
vanchinh16 <trinhvanchinh10122004@gmail.com>
2025-01-14 21:53:50 +0700

    gửi mail khi tạo voucher thành công

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-
+use App\Events\VoucherSuccess;
+use App\Listeners\VoucherSuccessNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,7 +19,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-
+          VoucherSuccess::class => [
+            VoucherSuccessNotification::class
+          ]
     ];
 
     /**
